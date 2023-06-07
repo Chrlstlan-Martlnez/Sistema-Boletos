fa95eaf8cc01176b546cbfc73d352cf05c18ee37
Christian Martínez <cmartinez.7525@unimar.edu.ve>
2023-06-06 23:19:24 -0400

    Tercer commit - Se añadió un if, junto con un repeat que verifica si el tipo de boleto ingresado por el usuario es valido

--- proyectosistemaboletos.pas
+++ proyectosistemaboletos.pas
@@ -1,6 +1,7 @@
 program Metro_Caracas;
 
-//uses crt;
+uses
+	crt;
 
 var 
 	Nombre, Apellido, Cedula : string;
@@ -34,59 +35,71 @@ begin
 				write('Cantidad de Boletos: '); readln(nBoletos);
 				
 			for i:=1 to nBoletos do
-					begin
-					//clrscr;//
-						writeln('Estos son los tipos de boletos que hay, elija los que desee.');
-						writeln('');
-						writeln('---------------------------------------------------------------------');
-						writeln('|         BOLETOS          |  COLOR  |       COBERTURA       |PRECIO|');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(1)Simple                 |Amarillo |1 viaje en metro       |  1   |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(2)Integrado              |Amarillo |1 viaje en metro       |  1   |');
-						writeln('|                          |         |1 viaje en bus         | 0.5  |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(3)Ida y Vuelta           |Amarillo |2 viajes en metro      |  2   |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(4)Ida y Vuelta Integrado |Amarillo |2 viajes en metro      |  3   |');
-						writeln('|                          |         |2 viajes en bus        |      |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(5)Multiabono             |Naranja  |10 viajes en metro     |  10  |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(6)Multiabono Integrado   |Naranja  |10 viajes en metro     |  15  |');
-						writeln('|                          |         |10 viajes en bus       |      |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(7)Estudiantil            |Azul     |10 viajes en metro     | 7.5  |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(8)Estudiantil Integrado  |Azul     |10 viajes en metro     |  15  |');
-						writeln('|                          |         |20 viajes en bus       |      |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(9)MetroTarjeta           |Rojo     |20 viajes              |  30  |');
-						writeln('|                          |         |30 viajes              |      |');
-						writeln('|                          |         |40 viajes              |      |');
-						writeln('|--------------------------|---------|-----------------------|------|');
-						writeln('|(10)MetroTarjeta Integrada|Rojo     |20 viajes(Metro y Bus) |  50  |');
-						writeln('|                          |         |30 viajes(Metro y Bus) |      |');
-						writeln('|                          |         |40 viajes(Metro y Bus) |      |');
-						writeln('---------------------------------------------------------------------');
+				begin
+					clrscr;
+					writeln('Estos son los tipos de boletos que hay, elija los que desee.');
+					writeln('');
+					writeln('---------------------------------------------------------------------');
+					writeln('|         BOLETOS          |  COLOR  |       COBERTURA       |PRECIO|');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(1)Simple                 |Amarillo |1 viaje en metro       |  1   |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(2)Integrado              |Amarillo |1 viaje en metro       |  1   |');
+					writeln('|                          |         |1 viaje en bus         | 0.5  |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(3)Ida y Vuelta           |Amarillo |2 viajes en metro      |  2   |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(4)Ida y Vuelta Integrado |Amarillo |2 viajes en metro      |  3   |');
+					writeln('|                          |         |2 viajes en bus        |      |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(5)Multiabono             |Naranja  |10 viajes en metro     |  10  |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(6)Multiabono Integrado   |Naranja  |10 viajes en metro     |  15  |');
+					writeln('|                          |         |10 viajes en bus       |      |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(7)Estudiantil            |Azul     |10 viajes en metro     | 7.5  |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(8)Estudiantil Integrado  |Azul     |10 viajes en metro     |  15  |');
+					writeln('|                          |         |20 viajes en bus       |      |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(9)MetroTarjeta           |Rojo     |20 viajes              |  30  |');
+					writeln('|                          |         |30 viajes              |      |');
+					writeln('|                          |         |40 viajes              |      |');
+					writeln('|--------------------------|---------|-----------------------|------|');
+					writeln('|(10)MetroTarjeta Integrada|Rojo     |20 viajes(Metro y Bus) |  50  |');
+					writeln('|                          |         |30 viajes(Metro y Bus) |      |');
+					writeln('|                          |         |40 viajes(Metro y Bus) |      |');
+					writeln('---------------------------------------------------------------------');
 						
-						writeln('Ingrese el numero correspondiente al tipo de boleto que desea comprar => '); readln(TipoBoleto);
-						write('Tu boleto numero ', i ,' es: ');
-						case TipoBoleto of
-							1: Writeln('Simple');
-							2: Writeln('Integrado');
-							3: Writeln('Ida y Vuelta');
-							4: Writeln('Ida y Vuelta Integrado');
-							5: Writeln('Multiabono');
-							6: Writeln('Multiabono Integrado');
-							7: Writeln('Estudiantil');
-							8: Writeln('Estudiantil Integrado');
-							9: Writeln('MetroTarjeta');
-							10: Writeln('MetroTarjeta Integrado');
+					writeln('Ingrese el numero correspondiente al tipo de boleto que desea comprar => '); readln(TipoBoleto);
+					
+					if ((TipoBoleto <> 1) and (TipoBoleto <> 2) and (TipoBoleto <> 3) and (TipoBoleto <> 4) and (TipoBoleto <> 5) 
+					and (TipoBoleto <> 6) and (TipoBoleto <> 7) and (TipoBoleto <> 8) and (TipoBoleto <> 9) and (TipoBoleto <> 10)) then
+							begin
+								repeat
+									writeln('Ingrese una opcion de boleto valida (1-2-3-4-5-6-7-8-9-10).');
+									readln(TipoBoleto);
+								until ((TipoBoleto = 1) or (TipoBoleto = 2) or (TipoBoleto = 3) or (TipoBoleto = 4) or (TipoBoleto = 5) 
+								or (TipoBoleto = 6) or (TipoBoleto = 7) or (TipoBoleto = 8) or (TipoBoleto = 9) or (TipoBoleto = 10));
+							end;
+					
+					
+					write('Tu boleto numero ', i ,' es: ');
+					case TipoBoleto of
+						1: Writeln('Simple');
+						2: Writeln('Integrado');
+						3: Writeln('Ida y Vuelta');
+						4: Writeln('Ida y Vuelta Integrado');
+						5: Writeln('Multiabono');
+						6: Writeln('Multiabono Integrado');
+						7: Writeln('Estudiantil');
+						8: Writeln('Estudiantil Integrado');
+						9: Writeln('MetroTarjeta');
+						10: Writeln('MetroTarjeta Integrado');
 						end;
-						writeln('Presiona Enter para continuar');
-						readln( );
-					end;
+					writeln('Presiona Enter para continuar');
+					readln( );
+				end;
 			end;
 		end;
 end.
